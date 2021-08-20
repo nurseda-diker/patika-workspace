b4f952f5a73d6e4e8514f13c34475bbd87c43889
nurseda diker <54947744+nurseda-diker@users.noreply.github.com>
2021-08-20 23:10:36 +0300

    Update sql-odev3.sql

--- sql-odev3.sql
+++ sql-odev3.sql
@@ -8,7 +8,7 @@ SELECT * FROM country WHERE country LIKE '_____%n';
 
 --film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
 --ILIKE operatörü LIKE operatörünün case - insensitive versiyonudur ve ILIKE yerine ~~* da kullanılabilir.
-SELECT title FROM film WHERE title ILIKE  '%t%t%t%t';
+SELECT title FROM film WHERE title ILIKE  '%t%t%t%t%';
 
 --film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 SELECT * FROM film WHERE title LIKE  'C%' AND length > 90 AND rental_rate = 2.99;
